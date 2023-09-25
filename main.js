c1fa871b7ba3de808ae9442849b7e094544b2421
Alberto Medina <79936222+albertomDev@users.noreply.github.com>
2023-09-25 19:54:32 +0100

    add fetch base

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-const searchBook = document.querySelector(".search-book");
+import { APIKey } from "./key.js";
+const searchBar = document.querySelector(".search-bar");
 const addBookButton = document.querySelector(".add-book-button");
+const searchBarForm = document.querySelector("#search-bar-form");
+
+const bookAPI = (term) => {
+  fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${term}&maxResults=20&key=${APIKey}`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("API error");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data.items);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 addBookButton.addEventListener("click", () => {
   addBookButton.hidden = true;
-  searchBook.hidden = false;
+  searchBar.hidden = false;
+  console.log("hello");
   setTimeout(() => {
-    searchBook.classList.add("clicked");
-    searchBook.focus();
+    searchBar.classList.add("clicked");
+    searchBar.focus();
   }, 100);
+});
+
+searchBarForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  bookAPI(searchBar.value);
 });
